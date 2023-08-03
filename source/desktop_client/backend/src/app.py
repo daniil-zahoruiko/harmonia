41fe751c5e1bb8ea149403d8240a4c02b2aebf68
Avdieienko <avdieienko.d@gmail.com>
2023-08-03 22:15:25 +0100

    Change test api format

--- source/desktop_client/backend/src/app.py
+++ source/desktop_client/backend/src/app.py
@@ -7,22 +7,25 @@ x = datetime.datetime.now()
 # Initializing flask app
 app = Flask(__name__)
 
-data =[
-        {
-            "title":"Test 1",
-            "file":"test1.mp3"
-        },
-        {
-            "title":"Test 2",
-            "file":"test2.mp3"
-        },
-        {
-            "title":"Test 3",
-            "file":"test3.mp3"
-        }
-    ]
+data ={"user":{
+    "songs":[{
+                "title":"Test 1",
+                "file":"test1.mp3"
+            },
+            {
+                "title":"Test 2",
+                "file":"test2.mp3"
+            },
+            {
+                "title":"Test 3",
+                "file":"test3.mp3"
+            }],
+    "playlists":["Playlist 1", "Playlist 2", "Playlist 3"]
+    }
+}
 
-songs = [song["file"] for song in data]
+
+songs = [song["file"] for song in data["user"]["songs"]]
 
 # Planned api routes: /api/artist/album/song/song.mp3
 # Planned api routes: /api/artist/info/.json
@@ -39,12 +42,14 @@ def get_time():
     # Returning an api for showing in  reactjs
     return jsonify(data)
 
-@app.route('/song/<name>')
+@app.route('/api/song/<name>')
 def song(name):
     if name not in songs:
         return "No such song"
     else:
         return send_file("./"+name)
+
+
 
 # Running app
 if __name__ == '__main__':
