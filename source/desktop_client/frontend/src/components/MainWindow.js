db62bf672cc62d7292419d4932b84020c2da6a1a
Avdieienko <avdieienko.d@gmail.com>
2023-08-23 13:35:02 +0100

    Update MainWindow.js

--- source/desktop_client/frontend/src/components/MainWindow.js
+++ source/desktop_client/frontend/src/components/MainWindow.js
@@ -1,12 +1,42 @@
 import { Home } from "./home/Home"
 import "../styles/mainWindow.css"
+import { Discover } from "./home/Discover"
+import React, {useContext} from "react"
+import { SongsContext } from "../SongsData"
+import { PlaylistView } from "./utils/PlaylistView"
 
 
 
-export const MainWindow = ({songs,currentIndex,setCurrentIndex}) => {
-    return(
-        <div className="main_window_wrapper">
-            <Home songs = {songs} currentIndex={currentIndex} setCurrentIndex={setCurrentIndex}/>
-        </div>
-    )
+
+export const MainWindow = () => {
+    const {page:[currentPage,setCurrentPage]} = useContext(SongsContext)
+    const {playlistRender:[showedPlaylist,setShowedPlaylist]} = React.useContext(SongsContext)
+
+    if(currentPage==="home"){
+        return(
+            <div className="main_window_wrapper">
+                <Home/>
+            </div>
+        )
+    }else if(currentPage === "discover"){
+        return(
+            <div className="main_window_wrapper">
+                <Discover/>
+            </div>
+        )
+    }else if(currentPage === "playlist-view"){
+        return(
+            <div className="main_window_wrapper">
+                <PlaylistView
+                owner={showedPlaylist.owner}
+                 type={showedPlaylist.type}
+                 name={showedPlaylist.name}
+                 description={showedPlaylist.description}
+                 songs = {showedPlaylist.songs}
+                 id = {showedPlaylist.id}
+                 />
+            </div>
+        )
+    }
+
 }
