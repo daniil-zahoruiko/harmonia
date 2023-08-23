3754e84966114bc05b80ef3121ff7ca4f7e6ce8f
Avdieienko <avdieienko.d@gmail.com>
2023-08-23 22:00:07 +0100

    Extended song player slider functionality

--- source/desktop_client/frontend/src/components/songPlayer/Player.js
+++ source/desktop_client/frontend/src/components/songPlayer/Player.js
@@ -1,37 +1,31 @@
-import React, { useEffect,useState } from 'react';
+import { useEffect,useState,useContext } from 'react';
 import {BsFillPlayCircleFill, BsFillPauseCircleFill, BsFillSkipStartCircleFill, BsFillSkipEndCircleFill,BsVolumeUp, BsVolumeMute,BsVolumeDown} from 'react-icons/bs';
 import { Loader } from '../utils/Loader';
-import '../../styles/player.css';
 import { SongsContext } from "../../SongsData";
+import '../../styles/player.css';
 
-export const Player = ({audioElem, currentSong, setCurrentSong,currentIndex})=> {
+export const Player = ({audioElem, currentSong})=> {
+
+  const { playlist:[currentPlaylist,],
+          playing:[isPlaying,],
+          songData:[currentSongData,setCurrentSongData],
+          song:[songLoaded, ],
+          toggles:[PlayPause] } = useContext(SongsContext)
 
   const [volume, setVolume] = useState(1)
-  const {playlist:[currentPlaylist,setCurrentPlaylist]} = React.useContext(SongsContext)
-  const {playing:[isPlaying,setIsPlaying]} = React.useContext(SongsContext)
-  const {songData:[currentSongData,setCurrentSongData]} = React.useContext(SongsContext)
+  const [slider,setSlider] = useState(currentSong.progress)
+  const [clicked, setClicked] = useState(false)
 
   const songs = currentPlaylist.songs
-
-  // useEffect(()=>{
-  //   console.log(songs)
-  //   songs = currentPlaylist.songs
-  // },[currentPlaylist])
-
-
-  // play/pause music
-  const PlayPause = ()=>
-  {
-    setIsPlaying(!isPlaying);
-
-  }
-
 
   // set current time while using range scroller
   const changeRange = (e) =>{
     const value = e.target.value
-    audioElem.current.currentTime = value/100 * currentSong.length
+    const res = value/100 * currentSong.length
+      audioElem.current.currentTime = res
+      setClicked(false)
   }
+
 
   const changeVolume = (e) =>{
     const value = e.target.value
@@ -40,89 +34,98 @@ export const Player = ({audioElem, currentSong, setCurrentSong,currentIndex})=> 
   }
 
   // skip to previous music
-  const skipBack = ()=>
-  {
-    if(!audioElem.current.currentTime){
-      return
-    }
+  const skipBack = ()=>{
+    if(!songLoaded) return
     if(audioElem.current.currentTime>3){
       audioElem.current.currentTime = 0;
       return
     }
-    const index = songs.findIndex(x=>x.title === currentSong.title);
+    const index = songs.findIndex(x=>x.title === currentSongData.title);
     if (index === 0)
     {
-      setCurrentSong(songs[songs.length - 1])
       setCurrentSongData(songs[songs.length - 1])
     }
     else
     {
-      setCurrentSong(songs[index - 1])
       setCurrentSongData(songs[index - 1])
     }
     audioElem.current.currentTime = 0;
   }
 
   // skip to next music
-  const skiptoNext =
-    ()=>
-  {
-    if(!audioElem.current.currentTime){
-      return
-    }
-    const index = songs.findIndex(x=>x.title === currentSong.title);
+  const skiptoNext =()=>{
+    if(!songLoaded) return
+
+    const index = songs.findIndex(x=>x.title === currentSongData.title);
 
     if (index === songs.length-1)
     {
-      setCurrentSong(songs[0])
       setCurrentSongData(songs[0])
     }
     else
     {
-      setCurrentSong(songs[index + 1])
       setCurrentSongData(songs[index + 1])
     }
     audioElem.current.currentTime = 0;
-    }
+  }
 
-  // Start next song after it ends
+  const toggle = (e) =>{
+    setClicked(true)
+    setSlider(e.target.value)
+  }
+
+    // Start next song after it ends
   useEffect(()=>{
-      if(audioElem.current){
-        if(audioElem.current.currentTime/currentSong.length*100 === 100){
-          skiptoNext()
-        }
+    if(audioElem.current){
+      if(audioElem.current.currentTime/currentSong.length*100 === 100){
+        skiptoNext()
       }
+    }
   })
 
+  useEffect(()=>{
+    if(!clicked){
+      setSlider(currentSong.progress)
+    }
+  },[currentSong])
 
-  // Load loader page until data is loaded
+  // // Load loader page until data is loaded
   if(audioElem.current === null) return <Loader/>
 
   return (
     <div className='player_container'>
       <div className='navigation_wrapper'>
         <div className='player_song_data'>
-          <img className='player_cover' alt={currentSong.title} src={`api/artist/${currentSong.id}/cover`} />
+          <img className='player_cover' alt={currentSongData.title} src={`api/artist/${currentSongData.id}/cover`} />
           <div className='player_song_text'>
-            <p className='player_title'>{currentSong.title}</p>
-            <p className='player_artist'>{currentSong.artist}</p>
+            <p className='player_title'>{currentSongData.title}</p>
+            <p className='player_artist'>{currentSongData.artist}</p>
           </div>
         </div>
+        {/*-------------------------------------------------------------
+        --------------------------SONG RANGE SLIDER BAR-------------------
+        -------------------------------------------------------------*/}
         <div className='playlist_timer_wrapper'>
-          <p>{Math.round(audioElem.current.currentTime%60)<10?`${Math.floor(audioElem.current.currentTime/60)}:0${Math.floor(audioElem.current.currentTime%60)}`:`${Math.floor(audioElem.current.currentTime/60)}:${Math.floor(audioElem.current.currentTime%60)}`}</p>
+          <p>{!currentSong.progress?"0:00":Math.floor(slider/100 * currentSong.length%60)<10
+          ?`${Math.floor(slider/100 * currentSong.length/60)}:0${Math.floor(slider/100 * currentSong.length%60)}`
+          :`${Math.floor(slider/100 * currentSong.length/60)}:${Math.floor(slider/100 * currentSong.length%60)}`}</p>
           <input
               type="range"
               className='seek_bar'
-              value={currentSong.length ? audioElem.current.currentTime/currentSong.length*100 : 0 }
-              onChange={changeRange}
+              value={slider?slider:0}
+              onChange={toggle}
               min="0"
               max="100"
               step="0.01"
+              onClickCapture={changeRange}
           />
           <p>{currentSong.length?
-          Math.round(currentSong.length%60)<10?`${Math.floor(currentSong.length/60)}:0${Math.floor(currentSong.length%60)}`:`${Math.floor(currentSong.length/60)}:${Math.floor(currentSong.length%60)}`
+          Math.floor(currentSong.length%60)<10?`${Math.floor(currentSong.length/60)}:0${Math.floor(currentSong.length%60)}`:`${Math.floor(currentSong.length/60)}:${Math.floor(currentSong.length%60)}`
           :"-:--"}</p>
         </div>
+        {/*-------------------------------------------------------------
+        --------------------------VOLUME SLIDER BAR-------------------
+        -------------------------------------------------------------*/}
         <div className='playlist_sound_wrapper'>
           {volume>0.7?<BsVolumeUp className='playlist_volume_svg'/>
           :volume>0?<BsVolumeDown className='playlist_volume_svg'/>
