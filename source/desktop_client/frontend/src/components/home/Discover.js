19a11dff6a13990a6275a95473ee647115b5b48c
Avdieienko <avdieienko.d@gmail.com>
2023-08-23 22:00:45 +0100

    Import restructure and include search bar

--- source/desktop_client/frontend/src/components/home/Discover.js
+++ source/desktop_client/frontend/src/components/home/Discover.js
@@ -1,16 +1,21 @@
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { SongsContext } from "../../SongsData";
 import { CategoryCard } from "../utils/CategoryCard"
 import "../../styles/discover.css"
 import { MainWindow } from "../MainWindow"
 import { Home } from "./Home"
+import { SearchBar } from "../searchbar/SearchBar";
+import { SearchResults } from "../searchbar/SearchResults";
 
 const categories = ["hip-hop","rock","rap","trap","classical","workout","jazz","indie","country"]
 
 export const Discover = () =>{
-    const {playlistRender:[showedPlaylist,setShowedPlaylist]} = useContext(SongsContext)
-    const {page:[currentPage,setCurrentPage]} = useContext(SongsContext)
-    const {db:[songs,loading]} = useContext(SongsContext)
+    const { db:[songs,loading],
+            playlistRender:[showedPlaylist,setShowedPlaylist],
+            page:[currentPage,setCurrentPage]   } = useContext(SongsContext)
+
+    const [result,setResult] = useState([])
+    const [input,setInput] = useState("")
 
     const changePlaylist = (owner,type,name,description,songs) =>{
         setShowedPlaylist({owner:owner,type:type,name:name,description:description,songs:songs,id:name})
@@ -18,12 +23,18 @@ export const Discover = () =>{
     }
 
     return(
-        <div className="genres_cards_wrapper">
+        <>
+            <SearchBar setResult={setResult} input={input} setInput={setInput}/>
+            {result.length
+            ?<SearchResults results={result} setInput={setInput} setResult={setResult}/>
+            :<div className="genres_cards_wrapper">
             {categories.map((category,key)=>{
                 const genre_songs = songs.filter((song)=> song.genre === category)
                 return <CategoryCard onClick={()=>changePlaylist("Project name","Public",category,`${category} description...`,genre_songs)} key={key} category={category}/>
             })}
-        </div>
+            </div>}
+
+        </>
     )
 
 }
