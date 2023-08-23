ae141735853402d87bf1302fea02cb5a8f046579
Avdieienko <avdieienko.d@gmail.com>
2023-08-23 22:02:04 +0100

    Implement skeleton loading to image

--- source/desktop_client/frontend/src/components/utils/LoadedImage.js
+++ source/desktop_client/frontend/src/components/utils/LoadedImage.js
@@ -1,13 +1,14 @@
-
+import {useState} from "react"
+import { ImageSkeleton } from "./Skeleton"
 
 
 export const LoadedImage = ({src, className, alt}) =>{
     const [loaded,setLoaded] = useState(false)
 
     return(
-        <>
-            {loaded?null:<div style={{backgroundColor:"red",height:"10vw",width:"10vw"}}/>}
+        <div className={className}>
+            {loaded?null:<ImageSkeleton/>}
             <img className={className} alt={alt} onLoad={()=>setLoaded(true)} style={loaded?{}:{display:"none"}} src={src} />
-        </>
+        </div>
     )
 }
--- source/desktop_client/frontend/src/components/utils/SongCard.js
+++ source/desktop_client/frontend/src/components/utils/SongCard.js
@@ -1,11 +1,12 @@
 import "../../styles/songcard.css"
+import { LoadedImage } from "./LoadedImage"
 
 
 export const SongCard = ({onClick, id, title, artist}) => {
 
     return(
         <div onClick={onClick} className="SongCard">
-            <img alt={title} src={`api/artist/${id}/cover/`} />
+            <LoadedImage className={"songcard_img"} alt={title} src={`api/artist/${id}/cover/`}/>
             <h1 className="song_card_title">{title}</h1>
             <p className="song_card_artist">{artist}</p>
         </div>
