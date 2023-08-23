561c4a6835497ca11433be407909e6ae32defd4c
Avdieienko <avdieienko.d@gmail.com>
2023-08-23 21:59:11 +0100

    Bug fix

--- source/desktop_client/frontend/src/components/Nav.js
+++ source/desktop_client/frontend/src/components/Nav.js
@@ -9,31 +9,31 @@ export const Nav = ({setPage}) =>{
     return(
         <nav className="desktop_nav">
             <ul>
-                <li onClick={()=>setCurrentPage("home")} className="nav_outer nav_logo">
+                <li className="nav_outer nav_logo">
                     <div>
-                        <p>Project name</p>
+                        <p onClick={()=>setCurrentPage("home")}>Project name</p>
                     </div>
                 </li>
-                <li onClick={()=>setCurrentPage("library")} className="nav_li nav_left">
-                    <p>
+                <li className="nav_li nav_left">
+                    <p onClick={()=>setCurrentPage("library")}>
                         Library
                     </p>
                 </li>
-                <li onClick={()=>setCurrentPage("user_account")}>
-                    <svg className="user_logo" xmlns="http://www.w3.org/2000/svg" width="52" height="52" viewBox="0 0 52 52" fill="none">
+                <li>
+                    <svg onClick={()=>setCurrentPage("user_account")} className="user_logo" xmlns="http://www.w3.org/2000/svg" width="52" height="52" viewBox="0 0 52 52" fill="none">
                         <path d="M21 18V16C21 13.2386 23.2386 11 26 11V11C28.7614 11 31 13.2386 31 16V18" stroke="white" strokeWidth="2"/>
                         <path d="M14 37V32.5C14 26.1487 19.1487 21 25.5 21V21C31.8513 21 37 26.1487 37 32.5V37" stroke="white" strokeWidth="2"/>
                         <path d="M26 51C39.8071 51 51 39.8071 51 26C51 12.1929 39.8071 1 26 1C12.1929 1 1 12.1929 1 26C1 39.8071 12.1929 51 26 51Z" stroke="white" strokeWidth="2"/>
                     </svg>
                 </li>
-                <li onClick={()=>setCurrentPage("discover")} className="nav_li nav_right">
-                    <p>
+                <li className="nav_li nav_right">
+                    <p onClick={()=>setCurrentPage("discover")}>
                         Discover
                     </p>
                 </li>
-                <li onClick={()=>setCurrentPage("settings")} className="nav_outer nav_settings">
+                <li className="nav_outer nav_settings">
                     <div>
-                        <IoSettingsOutline className="settings_svg"/>
+                        <IoSettingsOutline onClick={()=>setCurrentPage("settings")} className="settings_svg"/>
                     </div>
                 </li>
             </ul>
