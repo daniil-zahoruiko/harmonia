59f2f5f6e5b7dfd5be7266bab98dcba80fed3db0
Avdieienko <avdieienko.d@gmail.com>
2023-09-05 00:11:00 +0100

    Add form validation and minor structure changes

--- source/desktop_client/frontend/src/components/login/LogIn.js
+++ source/desktop_client/frontend/src/components/login/LogIn.js
@@ -2,6 +2,8 @@ import { useState, useContext } from "react"
 import { LogMeIn } from "../../api";
 import { UserContext } from "../../UserContext";
 import {useForm} from "react-hook-form"
+import { yupResolver } from '@hookform/resolvers/yup';
+import { logInSchema } from "../utils/ValidationSchemas";
 import {IoArrowBackCircleOutline} from "react-icons/io5"
 import {AiOutlineEye,AiOutlineEyeInvisible} from "react-icons/ai"
 import "../../styles/login.css"
@@ -20,7 +22,8 @@ export const LogIn = ({selectedAction, setSelectedAction}) =>
         defaultValues:{
             username: "",
             password: "",
-        }
+        },
+        resolver:yupResolver(logInSchema)
     })
 
     async function onSubmit(data)
@@ -40,23 +43,27 @@ export const LogIn = ({selectedAction, setSelectedAction}) =>
             <button className="back_wrapper" onClick={() => setSelectedAction("Main")}><IoArrowBackCircleOutline className="back_wrapper_svg"/></button>
             <form className="log_in_wrapper" onSubmit={handleSubmit(onSubmit)}>
                 <h1>¡Welcome back!</h1>
-                <div>
+                <div className="form_row">
                     <label htmlFor="username">
                         Username:
                     </label>
-                    <input className="login_input" id="username"  placeholder="Username" {...register("username")} />
+                    <div className="login_form_input_wrapper">
+                        <input className="login_input" id="username"  placeholder="Username" {...register("username")} />
+                    </div>
+                    <p className="form_error">{errors.username?.message}</p>
                 </div>
-                <div>
+                <div className="form_row">
                     <label htmlFor="password">
                         Password:
                     </label>
-                    <input className={`login_input ${passwordVisibility?"":"hide_pass"}`} id="password"  placeholder="Password" {...register("password")} />
-                    {passwordVisibility?<AiOutlineEye onClick={passwordToggle} className="pass_visibility"/>:<AiOutlineEyeInvisible onClick={passwordToggle} className="pass_visibility"/>}
+                    <div className="login_form_input_wrapper">
+                        <input className={`login_input ${passwordVisibility?"":"hide_pass"}`} id="password"  placeholder="Password" {...register("password")} />
+                        {passwordVisibility?<AiOutlineEye onClick={passwordToggle} className="pass_visibility"/>:<AiOutlineEyeInvisible onClick={passwordToggle} className="pass_visibility"/>}
+                    </div>
+                    <p className="form_error">{errors.password?.message}</p>
                 </div>
                 {error != null ? <p className="login_error">{error}</p> : null}
-                <div className="login_submit_button">
-                    <input id="submit" type="submit" value="Log In"/>
-                </div>
+                <input id="submit" type="submit" value="Log In"/>
             </form>
         </main>
     );
