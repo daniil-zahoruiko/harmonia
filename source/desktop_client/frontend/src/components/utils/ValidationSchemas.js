ad83fd4f3b94a1413ff0dc74407c065fa26dd69a
Avdieienko <avdieienko.d@gmail.com>
2023-09-05 00:10:03 +0100

    Add log in validation form schema

--- source/desktop_client/frontend/src/components/utils/ValidationSchemas.js
+++ source/desktop_client/frontend/src/components/utils/ValidationSchemas.js
@@ -15,15 +15,8 @@ export const signUpSchema = yup.object({
     fullName: yup.string().required("No full name provided")
 }).required();
 
-// export const logInSchema = yup.object({
-//     username: yup.string().required("No username provided."),
-//     password: yup.string()
-//         .required('No password provided.') 
-//         .min(8, 'Password is too short - should be 8 chars minimum.')
-//         .matches(/[a-zA-Z]/, 'Password can only contain Latin letters.'),
-//     passwordConfirmation: yup.string().oneOf([yup.ref("password"),null],"Passwords must match"),
-//     email: yup.string()
-//         .required("No email provided")
-//         .matches(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/, "Invalid email"),
-//     fullName: yup.string().required("No full name provided")
-// }).required();
+export const logInSchema = yup.object({
+    username: yup.string().required("No username provided."),
+    password: yup.string()
+        .required('No password provided.')
+}).required();
