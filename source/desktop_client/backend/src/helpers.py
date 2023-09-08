504582e950c33d98b9e614106b70db20f98ec60d
Daniil Zahoruiko <daniel.zagoruyko@gmail.com>
2023-09-08 12:29:56 -0600

    Mp3 song length backend

--- source/desktop_client/backend/src/helpers.py
+++ source/desktop_client/backend/src/helpers.py
@@ -1,3 +1,6 @@
+from io import BytesIO
+from mutagen import MP3
+
 def write_file(data, filename):
     with open(filename, 'wb') as f:
         f.write(data)
@@ -6,3 +9,10 @@ def read_file(filename):
     with open(filename, 'rb') as f:
         data = f.read()
     return data
+
+def get_mp3_length(blob):
+    data = BytesIO(blob)
+
+    audio = MP3(data)
+
+    return audio.info.length
